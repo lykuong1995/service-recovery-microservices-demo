fc61bf578f148a257e0f9ebd589a6b6ea79d822c
Kuong <lykuong1995@gmail.com>
2026-02-08 11:42:35 +0700

    Wire infra for payment service

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -1,3 +1,6 @@
 CREATE DATABASE auth_db OWNER lykuong;
 CREATE DATABASE order_db OWNER lykuong;
 CREATE DATABASE product_db OWNER lykuong;
+CREATE DATABASE inventory_db OWNER lykuong;
+CREATE DATABASE cart_db OWNER lykuong;
+CREATE DATABASE payment_db OWNER lykuong;
